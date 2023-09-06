7a6f7c26f86a5334f8fcdd2147bc8cc6b218f841
Utkarsh Singh <singhutkarsh5032@gmail.com>
2023-09-06 16:24:54 +0530

    Route Filter added

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-router.use("/admin/add-product", (req, res, next) => {
+router.use("/add-product", (req, res, next) => {
   res.send(
-    '<form action="/admin/add-product" method="post"><input type="text" name="title"/><button type="submit">Add Product</button></form>'
+    '<form action="/add-product" method="post"><input type="text" name="title"/><button type="submit">Add Product</button></form>'
   );
 });
 
-router.post("/admin/add-product ", (req, res, next) => {
+router.post("/add-product ", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
