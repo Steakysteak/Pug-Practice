812a2592e86075e183cdfeb66ad849124b474f39
Utkarsh Singh <singhutkarsh5032@gmail.com>
2023-09-06 15:19:32 +0530

    Second

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware 111111");
-  next();
+app.use("/add-product",(req, res, next) => {
+  console.log("In the Add Product Route");
+  res.send("<h1>The 'Add Product'</h1>")
 });
 
-app.use((req, res, next) => {
+app.use("/",(req, res, next) => {
   console.log("In the middleeware 2222222");
   res.send("<h1>Hello from express</h1>");
 });
